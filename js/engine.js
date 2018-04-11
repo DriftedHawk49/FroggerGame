f8d92c92eddef4f421205922a75d48d840a01275
Pulkit <spulkit489@gmail.com>
2018-04-11 14:07:02 +0530

    Minor BugFix & Comments
    Added the comments to The Javascript codes & Minro Bug fixes.

--- js/engine.js
+++ js/engine.js
@@ -1,94 +1,105 @@
-
+/*
+This is main game engine , but it is not a self calling function.
+It is called only when Player selection is complete.
+*/
 
 var Engine = function(global) {
-     $("#game-bar").toggleClass("hide");
-    var win = global.window,
-        canvas = $("#game-screen")[0],
-        ctx2 = canvas.getContext('2d');
+     $("#game-bar").toggleClass("hide");                        // Show the Game Status bar.
+    var win = global.window,                                    // Global Window object
+        canvas = $("#game-screen")[0],                          // select the game screen canvas element
+        ctx2 = canvas.getContext('2d');                         // Set the context of canvas to 2D
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = 505;                                         // Set the canvas Width
+    canvas.height = 606;                                        // Set the canvas Height
 
-     // This function is a loop;
+
+    /*
+    Main function ones called is called again and again to refresh the Frames
+    of the game , which is achieved by requestAnimationFrame() function.
+    */
     function main() {
-        update();
-        render();
-        checkEnd();
-        win.requestAnimationFrame(main);
+        update();                                               // Function which updates all the Elements on the canvas accordingly
+        render();                                               // Function which renders the objects on the canvas according to the frame
+        checkEnd();                                             // Function to check whether the game has ended.
+        win.requestAnimationFrame(main);                        // Call for the next frame.
     }
 
-    function init() {
+
+
+    function init() {                                           // Function which further calls the main() function when all the visual resources are loaded.
         main();
     }
 
     function update() {
-        $("#score")[0].innerText = `Score : ${score}`;
-        $("#best")[0].innerText = `Best : ${best}`;
-        updateEntities();
-        if(checkCollisions()){
-            player.reset();
-            $("#game-stat").addClass("oops");
-            setTimeout(function(){
+        $("#score")[0].innerText = `Score : ${score}`;          // update the score value on the Game Status Bar
+        $("#best")[0].innerText = `Best : ${best}`;             // update the Best score on the Game Status Bar
+        updateEntities();                                       // Update the Entities present on the canvas
+
+        if(checkCollisions()){                                  // If collision happens,
+            player.reset();                                     // Reset the Player Position
+            $("#game-stat").addClass("oops");                   // Display a Red bar over the Canvas signifying you collided
+            setTimeout(function(){                              // Remove that Bar after 500ms
             $("#game-stat").removeClass("oops");
-            },200);
-            if(lifenumber>=0)--lifenumber;
-            if(lifenumber===1)$("#life")[0].innerHTML=life1;
+            },500);
+            if(lifenumber>=0)--lifenumber;                      // If Life exists, reduce it by 1
+            if(lifenumber===1)$("#life")[0].innerHTML=life1;    // Update the Life numbers on the Game Status bar
             else if(lifenumber===2)$("#life")[0].innerHTML=life2;
             else if(lifenumber===3)$("#life")[0].innerHTML=life3;
             else if(lifenumber===0)$("#life")[0].innerHTML=" ";
-            else{
+            else{                                               // If no life is present , reset the level and scores.
             level=1;
             score=0;
-            $("#win")[0].innerText = `Level : ${level}`;
+            $("#win")[0].innerText = `Level : ${level}`;        // Update the Level
             }
         }
-        collected();
+        collected();                                            // Check whether some collectible is collected,
     }
 
-    function checkEnd() {
-        if(player.row===0) {  // If The game is completed , reset everything.
-            //Insert Sound of winning
-            $("#complete")[0].play();
-            norepeat.length=0;
-            $("#game-stat").addClass("hurray");
-            setTimeout(function(){
-            $("#game-stat").removeClass("hurray");
-            },200);
+    function checkEnd() {                                       // function to check the Ending(Winning , to be more specific)
 
-        setTimeout(function(pl){
+        if(player.row===0) {                                    // If The level is completed , reset everything.
+            $("#complete")[0].play();                           // Play the Sound of Level Completion
+            norepeat.length=0;                                  // Reset the no repeat Array so that the collectibles can be replaced.
+            $("#game-stat").addClass("hurray");                 // display a green bar over the canvas Signifying that Level is complete
+            setTimeout(function(){                              // remove the green bar after 500ms
+            $("#game-stat").removeClass("hurray");
+            },500);
+
+        setTimeout(function(pl){                                // Wait for 1 Frame , then Reset the Player position (60FPS is the standard , so 1 frame is loaded in 16.7 ms)
             pl.reset();
-        },15,player); // Wait for 1-2 frames , And then Reset.
-        level++; // Update the Wins
-        $("#win")[0].innerText = `Level : ${level}`;
-        $("#best")[0].innerText = `Best : ${best}`;
-        for(let addon of addon_array){
+        },15,player);
+        level++;                                                // Update the Level
+        $("#win")[0].innerText = `Level : ${level}`;            // Update the Level on Game Status Bar
+        $("#best")[0].innerText = `Best : ${best}`;             // Update the Best score on the Game status bar
+        for(let addon of addon_array){                          // Reset all the collectibles so that they appear at new, random postions
             addon.reset();
         }
         }
 
     }
-    function updateEntities(dt) {
+
+    function updateEntities() {                                 // Update Enemies and Player
         allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
+            enemy.update();
         });
         player.update();
     }
 
-    function collected(){
+    function collected(){                                       // To check whether a collectible is collected.
         for(let addon of addon_array){
-            if(addon.name!="rock"){
-            if(addon.row===player.row&&addon.col===player.col){
-                $("#collect")[0].play();
-                addon.x=undefined;
-                addon.y=undefined;
-                addon.row = undefined;
-                addon.col = undefined;
-                score = score+addon.points;
-                if(best<score)best=score;
-                if(addon.name=="health"&&lifenumber<3){
-                    if(lifenumber===-1)lifenumber=1;
-                    else ++lifenumber;
-                    if(lifenumber===1)$("#life")[0].innerHTML=life1;
+            if(addon.name!="rock"){                             // If the object is NOT rock , then ->
+            if(addon.row===player.row&&addon.col===player.col){ // If the position of collectible & player is same ,
+                $("#collect")[0].play();                        // Play the sound of collection
+                addon.x=undefined;                              // Empty the x-coordinate of the collectible
+                addon.y=undefined;                              // Empty the y-coordinate of the collectible
+                addon.row = undefined;                          // Empty the row of the collectible
+                addon.col = undefined;                          // Empty the column of the collectible, To make the collectible vanish from the canvas
+                score = score+addon.points;                     // Update the score according to the collectible value
+                if(best<score)best=score;                       // Update the best score if current score beats the best score.
+                if(addon.name=="health"&&lifenumber<3){         // If the collectible is health & health is actually less than three, then ->
+                    if(lifenumber===-1)lifenumber=1;            // If Health is -1 , then make it 1.
+                    else ++lifenumber;                          // Increment the Health.
+                    if(lifenumber===1)$("#life")[0].innerHTML=life1;    // Display the health icons according to the life number.
                     else if(lifenumber===2)$("#life")[0].innerHTML=life2;
                     else if(lifenumber===3)$("#life")[0].innerHTML=life3;
                 }
@@ -97,65 +108,64 @@ var Engine = function(global) {
         }
     }
 
-    function checkCollisions(){
-        for(let enemy of allEnemies){
-          if(player.row===enemy.row){
-            if(player.x+70>enemy.x&&enemy.x+70>player.x){
-                //Insert Sound of collission
-                $("#collide")[0].play();
-                return true;
+    function checkCollisions(){                                 // Function to check the player enemy collisions
+        for(let enemy of allEnemies){                           //Looping through the enemies,
+          if(player.row===enemy.row){                           // If player row & enemy row are same ,
+            if(player.x+70>enemy.x&&enemy.x+70>player.x){       // If the enemy touches the player OR player touches the enemy,
+                $("#collide")[0].play();                        // Play the sound of collision
+                return true;                                    // Return True , which signifies that the collision has happened.
             }
           }
         }
-        return false;
+        return false;                                           // If Nothing happens then return False , ( collision has not taken place)
     }
 
-    function render() {
-        var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+    function render() {                                         // Function to render all the Entities on the canvas.
+        var rowImages = [                                       // Array which contains all the image paths of watch to render.
+                'images/water-block.png',                       // Top row is water
+                'images/stone-block.png',                       // Row 1 of 3 of stone
+                'images/stone-block.png',                       // Row 2 of 3 of stone
+                'images/stone-block.png',                       // Row 3 of 3 of stone
+                'images/grass-block.png',                       // Row 1 of 2 of grass
+                'images/grass-block.png'                        // Row 2 of 2 of grass
             ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
-        ctx2.clearRect(0,0,canvas.width,canvas.height);
+            numRows = 6,                                        // number of rows,
+            numCols = 5,                                        // number of columns
+            row, col;                                           // Present Row and column
+        ctx2.clearRect(0,0,canvas.width,canvas.height);         // Clear the Present frame
 
-        for (row = 0; row < numRows; row++) {
+        for (row = 0; row < numRows; row++) {                   // Lopp to place all the objects accordingly.
             for (col = 0; col < numCols; col++) {
                 ctx2.drawImage(Resources.get(rowImages[row]), col * 101, row * 87);
             }
         }
-        for(let addon of addon_array){
+        for(let addon of addon_array){                          // Loop to place all the collectibles accordingly
             addon.render();
         }
-        renderEntities();
+        renderEntities();                                       // Function to Render Enemies and Player.
     }
 
     function renderEntities() {
-        allEnemies.forEach(function(enemy) {
+        allEnemies.forEach(function(enemy) {                    // Loop to render all the Enemies.
             enemy.render();
         });
-        player.render();
+        player.render();                                        // Loop to Render the Player
     }
 
 
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function(e) {                // A Keyup event Listener to control the player on canvas.
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);                 // Calls the handleInput Function of the player , sending in the keyname of the key pressed.
 });
 
 
-    Resources.load([
+    Resources.load([                                            // Load all the visual resources we need in game
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
@@ -168,6 +178,6 @@ document.addEventListener('keyup', function(e) {
         'images/Rock.png'
 
     ]);
-    Resources.onReady(init);
+    Resources.onReady(init);                                    // Call the init() function when all the visual resources are loaded.
     global.ctx2 = ctx2;
 };
--- js/selectionEngine.js
+++ js/selectionEngine.js
@@ -1,32 +1,50 @@
+
+/*
+Player Selection Engine.
+*/
+
 var engine = (function(global){
-let canvas = $("canvas")[0];
-let ctx = canvas.getContext("2d");
-ctx.font = "20px Helvetica";
+
+let canvas = $("canvas")[0];							// Select the Selection screen canvas element
+
+let ctx = canvas.getContext("2d");						// Set its context to 2D
+
+ctx.font = "20px Helvetica";							// Set the font of the Canvas
+
+
+/*
+Main function ones called is called again and again to refresh the Frames
+of the game , which is achieved by requestAnimationFrame() function.
+*/
+
+
 function main(){
-	render();
-	global.window.requestAnimationFrame(main);
+	render(); 											// Render the objects in the frame on the canvas
+	global.window.requestAnimationFrame(main);			// Call for the next frame.
 }
+
 function render(){
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.font = "40px Helvetica";
-	ctx.fillText("SELECT YOUR PLAYER",30,87*2);
-	ctx.font = "20px Helvetica";
-	selector.render();
-	for(let icon of iconArray){
+	ctx.clearRect(0,0,canvas.width,canvas.height);		// Clears the previous frame
+	ctx.font = "40px Helvetica";						// Set new font
+	ctx.fillText("SELECT YOUR PLAYER",30,87*2);			// Display Text Over the canvas
+	ctx.font = "20px Helvetica";						// Reset the font
+	selector.render();									// Render the Selector icon
+	for(let icon of iconArray){							// Render all the Player icons
 		icon.render();
 	}
-	for(let tag of tags){
+	for(let tag of tags){								// Render all the Improvised names of the players.
 		if(selector.col===tag.col){
 			tag.render();
 		}
 	}
-	ctx.font = "35px Helvetica";
-	ctx.fillText("PRESS ENTER TO SELECT",30,93*6);
+	ctx.font = "35px Helvetica";						// Reset the font.
+	ctx.fillText("PRESS ENTER TO SELECT",30,93*6);		// Display Text over the canvas
 }
-function init(){
+
+function init(){										// Function which further calls the main() function when all the visual resources are loaded.
 	main();
 }
-Resources.load([
+Resources.load([										// Load all the visual resources we need current canvas
 	"images/char-boy.png",
 	"images/char-cat-girl.png",
 	"images/char-horn-girl.png",
@@ -34,6 +52,6 @@ Resources.load([
 	"images/char-princess-girl.png",
 	"images/Selector.png"
 	]);
-Resources.onReady(init);
-global.ctx = ctx;
+Resources.onReady(init);								// calls init() function ones all the resources are loaded.
+global.ctx = ctx;										// change the global context to present context.
 })(this);
